099d805c4694a4bae0bcf585b4a53ab3bfdf13be
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-07-30 23:09:55 -0500

    weather

---
+++ keys.js
@@ -0,0 +1,2 @@
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+var queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
