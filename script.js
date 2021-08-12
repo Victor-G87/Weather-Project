f2892ab2804dbaa67a58404cfd102364a3ae787a
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-08-12 15:29:33 -0500

    update

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ moment().format('L');
 //-----------------------Search Function for Current City Weather----------------------------------//
 function searchCity(cityname) {
 
-
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
+    var queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
 
     $.ajax({
         url: queryURL,
